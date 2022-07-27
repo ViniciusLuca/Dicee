2c21a917c9b08d78914c7dfadad1512a6546458e
Viniciusluca <vini.luca17@gmail.com>
2022-07-27 17:26:32 -0400

    fix

--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -2,18 +2,32 @@
 //  ViewController.swift
 //  dicee
 //
-//  Created by user226569 on 7/27/22.
+//  Created by ViniciusLuca on 7/27/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+    
+    
+    @IBAction func pressedButton(_ sender: UIButton) {
+        
+        let dArray = [UIImage(named: "dice-six-faces-one"),
+                        UIImage(named: "dice-six-faces-two"),
+                        UIImage(named: "dice-six-faces-three"),
+                        UIImage(named: "dice-six-faces-four"),
+                        UIImage(named: "dice-six-faces-five"),
+                        UIImage(named: "dice-six-faces-six")]
+                        
+        diceImage1.image = dArray[Int.random(in: 0...5)]
+        diceImage2.image = dArray[Int.random(in: 0...5)]
+                         
     }
-
-
+    
 }
 
